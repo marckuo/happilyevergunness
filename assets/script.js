95f0a665ed86674bca92dac7020cbfee3d322e7f
Marc Kuo <marc.kuo@gmail.com>
2017-04-08 17:51:49 -0400

    Add mobile nav

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
@@ -20,7 +20,13 @@ $(document).ready(function(){
         window.location.hash = hash;
       });
     }
+  });
 
-     // End if
+  $("#nav--hamburger-button").on('click', function(event) {
+    $('.nav--mobile').slideToggle();
+  });
+
+  $(".nav--mobile--link").on('click', function(event) {
+    $('.nav--mobile').slideToggle();
   });
 });
